9cb7f567cda8546c268bccd949d9beccbd516b86
KubaM381 <th9nfz6r4h@privaterelay.appleid.com>
2026-09-10 18:49:47 +0200

    Load shared history service in builder bootstrap

--- js/builder.js
+++ js/builder.js
@@ -3,5 +3,6 @@
 // The DOMContentLoaded lifecycle stays unchanged during the safe migration.
 document.write('<script src="js/state.js"><\/script>');
 document.write('<script src="js/storage.js"><\/script>');
+document.write('<script src="js/history.js"><\/script>');
 document.write('<script src="js/legacy-bridge.js"><\/script>');
 document.write('<script src="js/builder-core.js"><\/script>');
