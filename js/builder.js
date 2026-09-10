439bd9094c9cede870b9b965a8a7f64e4c83f34f
KubaM381 <th9nfz6r4h@privaterelay.appleid.com>
2026-09-10 18:47:27 +0200

    Load legacy bridge before editor core

--- js/builder.js
+++ js/builder.js
@@ -3,4 +3,5 @@
 // The DOMContentLoaded lifecycle stays unchanged during the safe migration.
 document.write('<script src="js/state.js"><\/script>');
 document.write('<script src="js/storage.js"><\/script>');
+document.write('<script src="js/legacy-bridge.js"><\/script>');
 document.write('<script src="js/builder-core.js"><\/script>');
