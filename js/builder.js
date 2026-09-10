dc432cc70125321cd82db65d95a5c2193ba28c73
KubaM381 <th9nfz6r4h@privaterelay.appleid.com>
2026-09-10 23:31:40 +0200

    feat: load inspector properties runtime

--- js/builder.js
+++ js/builder.js
@@ -13,6 +13,7 @@ document.write('<script src="js/canvas-runtime.js"><\/script>');
 document.write('<script src="js/elements.js"><\/script>');
 document.write('<script src="js/inspector.js"><\/script>');
 document.write('<script src="js/inspector-actions-runtime.js"><\/script>');
+document.write('<script src="js/inspector-properties-runtime.js"><\/script>');
 document.write('<script src="js/toolbar.js"><\/script>');
 document.write('<script src="js/header-footer.js"><\/script>');
 document.write('<script src="js/products.js"><\/script>');
