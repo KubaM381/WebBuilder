8681a2e5bab158d11eca9b6b7b88f2854d7c83dd
KubaM381 <th9nfz6r4h@privaterelay.appleid.com>
2026-09-10 18:54:20 +0200

    Load canvas service in builder bootstrap

--- js/builder.js
+++ js/builder.js
@@ -4,5 +4,6 @@
 document.write('<script src="js/state.js"><\/script>');
 document.write('<script src="js/storage.js"><\/script>');
 document.write('<script src="js/history.js"><\/script>');
+document.write('<script src="js/canvas.js"><\/script>');
 document.write('<script src="js/legacy-bridge.js"><\/script>');
 document.write('<script src="js/builder-core.js"><\/script>');
