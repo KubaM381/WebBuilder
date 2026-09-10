bc93ae7d0ef00fe3d4650c814902bc7724e54894
KubaM381 <th9nfz6r4h@privaterelay.appleid.com>
2026-09-10 19:14:04 +0200

    Load runtime health check in builder bootstrap

--- js/builder.js
+++ js/builder.js
@@ -13,4 +13,5 @@ document.write('<script src="js/cart.js"><\/script>');
 document.write('<script src="js/modals.js"><\/script>');
 document.write('<script src="js/preview.js"><\/script>');
 document.write('<script src="js/legacy-bridge.js"><\/script>');
+document.write('<script src="js/runtime-check.js"><\/script>');
 document.write('<script src="js/builder-core.js"><\/script>');
